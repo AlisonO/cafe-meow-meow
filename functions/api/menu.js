9aa556430ea4c78a983287760a1992776fc7940d
Alison O <ali.oatley@gmail.com>
2024-07-02 12:35:34 -0500

    testing

--- functions/api/menu.js
+++ functions/api/menu.js
@@ -3,5 +3,5 @@ export async function onRequest(context) {
   
     const answer = await context.env.Baristas.run('@cf/meta/llama-3-8b-instruct', input);
   
-    return Response.json(answer.Response);
+    return Response.json(answer).response;
 }
